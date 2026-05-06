852a8e444d729220fb0f1fad78a16b55bd87686c
Kenneth Rafael Ellison <tiberius@Mac.lan>
2026-05-06 13:42:03 -0700

    Allow browser private relay access

--- multiplayer_relay.py
+++ multiplayer_relay.py
@@ -236,6 +236,7 @@ class RelayHandler(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Allow-Private-Network", "true")
         self.send_header("Cache-Control", "no-store")
         self.send_header("Content-Length", str(len(raw)))
         self.end_headers()
